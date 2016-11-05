2a4fb8fb1e1fcf5c85fa15b2edb7e4cc1a36f938
omkar-dsd <omkar-dsd@gitlab.com>
2016-11-05 23:03:08 +0530

    redundant files removed

---
+++ memory_game/config.js
@@ -0,0 +1,102 @@
+// Copyright 2015-2016, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+// Hierarchical node.js configuration with command-line arguments, environment
+// variables, and files.
+const nconf = module.exports = require('nconf');
+const path = require('path');
+
+// Memcache configuration settings
+const MEMCACHE_HOST = process.env.MEMCACHE_PORT_11211_TCP_ADDR || 'localhost';
+const MEMCACHE_PORT = process.env.MEMCACHE_PORT_11211_TCP_PORT || 11211;
+
+nconf
+  // 1. Command-line arguments
+  .argv()
+  // 2. Environment variables
+  .env([
+    'CLOUD_BUCKET',
+    'DATA_BACKEND',
+    'GCLOUD_PROJECT',
+    'MONGO_URL',
+    'MONGO_COLLECTION',
+    'MYSQL_USER',
+    'MYSQL_PASSWORD',
+    'MYSQL_HOST',
+    'NODE_ENV',
+    'OAUTH2_CLIENT_ID',
+    'OAUTH2_CLIENT_SECRET',
+    'OAUTH2_CALLBACK',
+    'PORT',
+    'SECRET'
+  ])
+  // 3. Config file
+  .file({ file: path.join(__dirname, 'config.json') })
+  // 4. Defaults
+  .defaults({
+    // Typically you will create a bucket with the same name as your project ID.
+    CLOUD_BUCKET: 'fit-force-148609',
+
+    // dataBackend can be 'datastore', 'cloudsql', or 'mongodb'. Be sure to
+    // configure the appropriate settings for each storage engine below.
+    // If you are unsure, use datastore as it requires no additional
+    // configuration.
+    DATA_BACKEND: 'datastore',
+
+    // This is the id of your project in the Google Cloud Developers Console.
+    GCLOUD_PROJECT: 'Memory Game',
+
+    // Connection url for the Memcache instance used to store session data
+    MEMCACHE_URL: `${MEMCACHE_HOST}:${MEMCACHE_PORT}`,
+
+    // MongoDB connection string
+    // https://docs.mongodb.org/manual/reference/connection-string/
+    MONGO_URL: 'mongodb://localhost:27017',
+    MONGO_COLLECTION: 'books',
+
+    MYSQL_USER: '',
+    MYSQL_PASSWORD: '',
+    MYSQL_HOST: '',
+
+    OAUTH2_CLIENT_ID: '901294411832-lfe0k4i3946os8e4cofd95d1t95qg580.apps.googleusercontent.com',
+    OAUTH2_CLIENT_SECRET: 'SbOacR4DpNDfA0xmGiIU5LHr',
+    OAUTH2_CALLBACK: 'http://localhost:8080/auth/google/callback',
+
+    // Port the HTTP server
+    PORT: 8080,
+
+    SECRET: 'keyboardcat'
+  });
+
+// Check for required settings
+checkConfig('GCLOUD_PROJECT');
+checkConfig('CLOUD_BUCKET');
+checkConfig('OAUTH2_CLIENT_ID');
+checkConfig('OAUTH2_CLIENT_SECRET');
+
+if (nconf.get('DATA_BACKEND') === 'cloudsql') {
+  checkConfig('MYSQL_USER');
+  checkConfig('MYSQL_PASSWORD');
+  checkConfig('MYSQL_HOST');
+} else if (nconf.get('DATA_BACKEND') === 'mongodb') {
+  checkConfig('MONGO_URL');
+  checkConfig('MONGO_COLLECTION');
+}
+
+function checkConfig (setting) {
+  if (!nconf.get(setting)) {
+    throw new Error(`You must set ${setting} as an environment variable or in config.json!`);
+  }
+}
---
+++ memory_game/config/auth.js
@@ -0,0 +1,12 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'googleAuth' : {
+        'clientID'      : '901294411832-lfe0k4i3946os8e4cofd95d1t95qg580.apps.googleusercontent.com',
+        'clientSecret'  : 'SbOacR4DpNDfA0xmGiIU5LHr',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
